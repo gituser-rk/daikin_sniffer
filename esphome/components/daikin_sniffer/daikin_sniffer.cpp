a80d2a35af41987bc48256fcc1ecb5aaf6f7dbce
gituser-rk <42944375+gituser-rk@users.noreply.github.com>
2026-02-19 22:47:32 +0100

    Update daikin_sniffer.cpp

--- esphome/components/daikin_sniffer/daikin_sniffer.cpp
+++ esphome/components/daikin_sniffer/daikin_sniffer.cpp
@@ -1,24 +1,24 @@
 #include "daikin_sniffer.h"
 #include "esphome/core/log.h"
 
+namespace esphome {
 namespace daikin_sniffer {
 
 static const char *TAG = "daikin_sniffer";
 
-DaikinSniffer::DaikinSniffer(esphome::uart::UARTComponent *parent)
-    : esphome::Component(), esphome::uart::UARTDevice(parent) {
-}
-
+DaikinSniffer::DaikinSniffer(uart::UARTComponent *parent)
+    : Component(), UARTDevice(parent) {}
 
 void DaikinSniffer::setup() {
   ESP_LOGI(TAG, "Daikin sniffer initialized");
 }
 
 void DaikinSniffer::loop() {
-  while (available()) {
-    uint8_t b = read();
+  while (this->available()) {
+    uint8_t b = this->read();
     ESP_LOGD(TAG, "RX: %02X", b);
   }
 }
 
 }  // namespace daikin_sniffer
+}  // namespace esphome
