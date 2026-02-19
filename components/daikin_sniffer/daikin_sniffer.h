12a6e98759b66528cd237c6538b021cdfab28677
gituser-rk <42944375+gituser-rk@users.noreply.github.com>
2026-02-19 20:48:20 +0100

    Update daikin_sniffer.h

--- components/daikin_sniffer/daikin_sniffer.h
+++ components/daikin_sniffer/daikin_sniffer.h
@@ -1,11 +1,16 @@
 #pragma once
-#include "esphome.h"
+
+#include "esphome/core/component.h"
+#include "esphome/components/uart/uart.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+
+namespace daikin_sniffer {
 
 class DaikinSniffer : public esphome::Component, public esphome::uart::UARTDevice {
  public:
   esphome::text_sensor::TextSensor *sensor;
 
-  DaikinSniffer(esphome::uart::UARTComponent *parent);
+  explicit DaikinSniffer(esphome::uart::UARTComponent *parent);
 
   void setup() override;
   void loop() override;
@@ -13,3 +18,5 @@ class DaikinSniffer : public esphome::Component, public esphome::uart::UARTDevic
  private:
   std::vector<uint8_t> buffer;
 };
+
+}  // namespace daikin_sniffer
